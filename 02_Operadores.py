af85cea2cc9a52d6a0e26e3331fb1c9be6927d90
Gorka Sánchez Arregui <72950060+Gorka7@users.noreply.github.com>
2024-12-09 18:01:20 +0100

    Add files via upload

---
+++ 02_Operadores.py
@@ -0,0 +1,17 @@
+
+num1 = 3
+num2 = 4
+
+#Operadores
+print(num1+num2) #3+4
+print(num1-num2) #3-4
+print(num1*num2) #3x4
+print(num1/num2) #3/4 
+print(10 % 3)
+print(10 // 3) #Sacar un numero entero de una división
+print(2**3) #Exponente 2 a la 3
+
+#Operadores Comparadores
+print(3>4)
+print(3<4)
+print(34)
